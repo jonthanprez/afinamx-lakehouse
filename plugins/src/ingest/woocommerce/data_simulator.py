a63657caa1a0b805e1f4d70730980fd86deb520e
Jonathan <jonthanprez@gmail.com>
2026-07-21 21:01:48 -0600

    The file tree is set up to start the WooCommerce ingest

--- plugins/src/ingest/woocommerce/data_simulator.py
+++ plugins/src/ingest/woocommerce/data_simulator.py
@@ -3,12 +3,5 @@ import logging
 import json
 import os
 from datetime import datetime
-from faker import Faker
 
-from src.ingest.woocommerce.products import PRODUCTS_CATALOG, METHODS_PAYMENT, ORDER_STATUS
-
-logger = logging.getLogger("airflow.task")
-fake = Faker(['es_MX'])
-
-def generate_json_order(order_id: int) -> dict:
     
